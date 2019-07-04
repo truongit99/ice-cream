01f2cc771a44c164c0d45311975d632c1a19cde9
Truong <tgv99hy@gmail.com>
2019-07-05 01:19:23 +0700

    crete folder
    tạo folder

---
+++ src/ice-cream/ice-cream/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ice_cream.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
